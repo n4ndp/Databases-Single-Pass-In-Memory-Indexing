793b56d698aa3b3a0ba5521e1fdd2a30b2805f85
n4ndp <fernando.adriano.choqque.mejia@gmail.com>
2023-11-18 05:56:52 +0000

    Queso ft. Mike Southside, Neo Pistea

--- source/preprocessor.py
+++ source/preprocessor.py
@@ -11,37 +11,35 @@ from nltk.stem import PorterStemmer
 from paths import DATA_DIR
 
 class Preprocessor:
-    def __init__(self, file_name_data, file_name_preprocessed, stop_words=False):
+    def __init__(self, file_name_data, stop_words=False):
         """
-        file_name_data: the name of the file containing the data
-        file_name_preprocessed: the name of the file to save the preprocessed data
+        file_name_data: the name of the file containing the data (csv)
         stop_words: a boolean indicating whether to remove stop words
         """
         self.file_name_data = file_name_data
-        self.file_name_preprocessed = file_name_preprocessed
 
-        self.stop_words = set(stopwords.words("english")) if stop_words else None
-        self.word_tokenize = word_tokenize
-        self.ps = PorterStemmer()
+        self.stop_words = set(stopwords.words("english")) if stop_words else None # Set of stop words
+        self.word_tokenize = word_tokenize # Function for tokenizing words
+        self.ps = PorterStemmer() # Stemmer
 
-    def _preprocess(self, text):
+    def _preprocess(self, id, content):
         """Preprocess the text by tokenizing, removing stop words, and stemming."""
-        tokens = [word.lower() for word in self.word_tokenize(text)]
+        tokens = [word.lower() for word in self.word_tokenize(content)] # Tokenize the content of the song and convert to lowercase
+
         if self.stop_words: # Remove stop words
             tokens = [word for word in tokens if word not in self.stop_words]
 
-        tokens = [word for word in tokens if re.match(r'^[A-Za-z]+$', word)]
-        tokens = [self.ps.stem(word) for word in tokens]
+        tokens = [word for word in tokens if re.match(r'^[A-Za-z]+$', word)] # Remove non-alphabetic characters
 
-        return tokens
+        for token in tokens:
+            token = self.ps.stem(token)
+            yield (id, token) # Return a tuple of the id and the token
 
-    def _preprocess_data(self):
-        """Preprocess the data and save it as a JSON file."""
+    def preprocess(self):
+        """Preprocess the data"""
         data = pd.read_csv(DATA_DIR + self.file_name_data)
-        processed_data = [] # A list of dictionaries containing the processed data for each song
 
         for index, row in data.iterrows():
-            track_id = row["track_id"]
             track_name = row["track_name"]
             track_artist = row["track_artist"]
             lyrics = row["lyrics"]
@@ -49,23 +47,17 @@ class Preprocessor:
             playlist_name = row["playlist_name"]
             playlist_genre = row["playlist_genre"]
 
-            text = track_name + " " + track_artist + " " + lyrics + " " + track_album_name + " " + playlist_name + " " + playlist_genre # Combine all the text into one string
-            tokens = self._preprocess(text)
+            content = track_name + " " + track_artist + " " + lyrics + " " + track_album_name + " " + playlist_name + " " + playlist_genre # Combine all the columns into one string
+            track_id = row["track_id"] # The id of the song
 
-            # Create a dictionary for each song
-            song_data = {
-                "track_id": track_id,
-                "track_processed": tokens
-            }
+            for tuple_id_token in self._preprocess(track_id, content):
+                yield tuple_id_token # Return a tuple of the id and the token
 
-            processed_data.append(song_data)
+    def token_stream(self):
+        """Generates a stream of tokens"""
+        for tuple_id_token in self.preprocess():
+            yield tuple_id_token  # Emits each token tuple produced by preprocess()
 
-        # Save the processed data as a JSON file in the data directory
-        with open(DATA_DIR + self.file_name_preprocessed, 'w') as file:
-            json.dump(processed_data, file)
-
-    def preprocess(self):
-        self._preprocess_data()
 
 if __name__ == "__main__": # Example usage
     all_songs = pd.read_csv(DATA_DIR + "spotify_songs.csv")
@@ -73,11 +65,10 @@ if __name__ == "__main__": # Example usage
     selected_columns = ["track_id", "track_name", "track_artist", "lyrics", "track_album_name", "playlist_name", "playlist_genre"] # Only use these columns
     filtered_english_songs = english_songs[selected_columns]
     test = filtered_english_songs.head(3) # Only use the first 3 songs for testing
-    test.to_csv(DATA_DIR + "test.csv", index=False)
+    test.to_csv(DATA_DIR + "spotify_songs_es_3.csv", index=False)
 
-    preprocessor = Preprocessor("test.csv", "test.json", stop_words=True)
-    preprocessor.preprocess()
+    preprocessor = Preprocessor("spotify_songs_es_3.csv", stop_words=True)
 
-    with open(DATA_DIR + "test.json") as file:
-        processed_data = json.load(file)
-        print(len(processed_data)) # Should be 3
+    # Iterate through the token stream and print tokens
+    for track_id, token in preprocessor.token_stream():
+        print(f"Track ID: {track_id}, Token: {token}")
